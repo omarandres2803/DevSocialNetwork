f8186b771a82992e5042470a4a273bac4b37ca8a
omarandres2803 <omarandres2803@gmail.com>
2022-02-14 00:03:51 +0100

    Added Routes initial configuration

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
     res.send('API Running...');
 })
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 app.listen(PORT, () => console.log(`💻 Server started on port ${PORT} 👌 !`));
 
